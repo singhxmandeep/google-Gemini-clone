6aaccca4f553b879ff983a26b2928e6c1e64d8e2
Mandeep Singh <mandeepsingh.71ms31@gmail.com>
2024-06-20 10:10:04 -0400

    gemini clone project 04

---
+++ src/components/main/main.jsx
@@ -0,0 +1,96 @@
+import React, { useContext } from "react";
+import { assets } from "../../assets/assets";
+import "./main.css";
+import { Context } from "../../context/context";
+
+const main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Mandeep,</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Help me find YouTube videos to care for a specific plant</p>
+                <img src={assets.youtube_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Create Vibrant & playful image with lots of details</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Write code for a specific task, icluding edge cases</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Give me tips for how to grow my youtube channel</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a promt here"
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              {input?<img onClick={() => onSent()} src={assets.send_icon} alt="" />:null}
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses. Your privacy and Gemini Apps
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default main;
